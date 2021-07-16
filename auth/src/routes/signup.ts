ad76fa8efa23dd17c6fc2cf71037fe9b3fa1d87b
VU NGUYEN <vunguyen@VUs-MacBook-Pro.lan>
2021-07-16 16:28:49 -0400

    update auth with cookie session

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validator-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -34,6 +36,18 @@ router.post('/api/users/signup', [
         const user = User.build({ email, password });
         await user.save();
 
+        // Generate JwT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'asdf');
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        console.log('session cookie ', req.session)
         res.status(201).send(user);
 });
 
