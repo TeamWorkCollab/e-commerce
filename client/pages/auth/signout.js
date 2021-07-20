28764ccf85bff6053e06e091386c49f433fd7a83
Elaine Alasagas <ealasagas@gmail.com>
2021-07-20 02:21:21 -0700

    added sass, signin, signout, signup pgs, and bootstrap

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+//KNOWN ERROR FOR MAPPED ERRORS
+const SignOut = () => {
+    const { doRequest } = useRequest({
+        url: '/api/users/signout',
+        method: 'post',
+        body: {},
+        onSuccess: () => Router.push('/')
+      });
+    
+      useEffect(() => {
+        doRequest();
+      }, []);
+    
+      return (
+        <div>Signing you out...</div>
+      );
+};
+
+export default SignOut;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import HomeNav from '../components/navbar';
+import styles from '../styles/home.module.scss';
+
+
 const Landing = () => {
-    return <h1>Landing Page Test</h1>
-}
+    return (
+        <div className={styles.container}>
+            <HomeNav />
+            <h1>Landing Page Test</h1>
+            <div className={styles.test}>TEST ME IN SASS</div>
+        </div>
+    );
+};
 
 export default Landing;
